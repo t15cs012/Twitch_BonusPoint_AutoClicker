854907e596cc1bd5598e0dcc8e431661dd51b6ef
t15cs012 <study.t15cs012@gmail.com>
2020-06-20 14:33:06 +0900

    [fix] 別チャンネルに遷移したときの判定がうまく行かない不具合を修正

--- myscript.js
+++ myscript.js
@@ -17,21 +17,53 @@ function onClickBonusPoint() {
 }
 
 
-// 監視
+// ボーナスポイントボタンの監視
 const observer = new MutationObserver(onClickBonusPoint)
 
 
-// tw-full-height tw-relative tw-z-aboveが存在するまで探し続ける
-var timer = setInterval(() => {
-    var bonuspoint_elem = document.getElementsByClassName('tw-full-height tw-relative tw-z-above')
-    if (bonuspoint_elem.length > 0) {
-        console.log("Found target")
-        clearInterval(timer)
-        onClickBonusPoint()
-        observer.observe(bonuspoint_elem[0], {
-            childList: true,
-            subtree: true,
-            attributes: false
-        })
-    }
-}, 10000);
+// 初期化
+function init() {
+    observer.disconnect()
+
+    // tw-full-height tw-relative tw-z-aboveが存在するまで探し続ける
+    var timer = setInterval(() => {
+        var bonuspoint_elem = document.getElementsByClassName('tw-full-height tw-relative tw-z-above')
+        if (bonuspoint_elem.length > 0) {
+            console.log("Found target")
+            clearInterval(timer)
+            onClickBonusPoint()
+            observer.observe(bonuspoint_elem[0], {
+                childList: true,
+                subtree: true,
+                attributes: false
+            })
+        }
+    }, 1000);
+}
+
+
+// 読み込み時のHref
+var oldHref = document.location.href;
+
+// 読み込まれた際の処理
+window.addEventListener('load', (event) => {
+    this.console.log("onload")
+    init();
+
+    // URL遷移の監視
+    var bodyList = document.querySelector("body");
+    var observer_url = new MutationObserver((mutations) => {
+        mutations.forEach(() => {
+            if (oldHref != document.location.href) {
+                console.log("Change URL. Restart init.")
+                oldHref = document.location.href;
+                init();
+            }
+        });
+    });
+
+    observer_url.observe(bodyList, {
+        childList: true,
+        subtree: true
+    });
+});
